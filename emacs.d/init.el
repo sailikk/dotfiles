79f59b739db0dc6834cc1250bdca40363abda92d
Sailik Kundu <sailikkundu@gmail.com>
2020-11-19 16:51:50 -0500

    Use use-package everywhere, and added a couple more modes for my use
    1. Actually use use-package everywhere: try to remove (require ...) everywhere
    2. org-journal mode added
    3. dashboard mode added
    4. Ivy mode added

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,5 @@
-;; ;; -*- mode: elisp -*-
-; (add-to-list 'load-path "~/.emacs.d/vendor/use-package")
 (require 'package)
+(package-initialize)
 (setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
                          ("melpa"     . "http://melpa.org/packages/")))
 
@@ -38,13 +37,16 @@
   (find-file user-init-file))
 (global-set-key (kbd "C-c i") 'open-user-init-file)
 
-(set-face-attribute 'default nil :height 95) ; Font size
+(set-face-attribute 'default nil :height 105)                 ;; Font size
 (if (window-system) (set-frame-size (selected-frame) 180 40)) ;; Initial Frame Size
 
 ;; Very Minor Modes (?)
 (show-paren-mode 1) ; highlight matching parentheses
 (savehist-mode 1)
+(global-hi-lock-mode 1)
+(transient-mark-mode 1) ;; Enable transient mark mode
 
+;; Basic Quality of Life Settings
 (setq auto-window-vscroll nil)
 (xterm-mouse-mode 1) ; enable mouse support in terminal
 ;; better mouse scroll
@@ -59,43 +61,35 @@
 (setq-default indent-tabs-mode nil) ; use spaces instead of tabs
 (setq inhibit-splash-screen t) ;; inhibit splash screen at start
 
-;; Not sure what these do or if they're useful
-;; (setq load-prefer-newer t)
-;; (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(use-package dashboard
+  :init
+  (add-hook 'after-init-hook 'dashboard-refresh-buffer)
+  :config
+  (setq dashboard-startup-banner 'logo)
+  (dashboard-setup-startup-hook))
 
-;; ;; Enable transient mark mode
-(transient-mark-mode 1)
-
-;;;;Org mode configuration
-;; Enable Org mode
-(require 'org)
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c c") 'org-capture)
-(setq org-agenda-window-setup 'other-window)
-(setq org-agenda-restore-windows-after-quit t)
-(setq org-agenda-show-future-repeats nil)
+;; Org mode configuration
+(use-package org
+  :bind
+  (("C-c l" . org-store-link)
+   ("C-c a" . org-agenda)
+   ("C-c c" . org-capture))
+  :config
+  (setq org-agenda-window-setup 'other-window
+        org-agenda-restore-windows-after-quit t
+        org-agenda-show-future-repeats nil))
 (require 'org-mouse)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-custom-commands
-   '(("n" "Agenda and all TODOs"
-      ((agenda "" nil)
-       (alltodo "" nil))
-      nil)
-     ("i" "Important: Urgent things and phone calls"
-      ((tags "URGENT"
-             ((org-agenda-overriding-header "Urgent things to do")))
-       (tags "PHONE"
-             ((org-agenda-overriding-header "Phone calls to do"))))
-      nil nil)))
- '(org-agenda-files '("~/org/school.org"))
- '(org-log-into-drawer t)
- '(package-selected-packages
-   '(ivy rainbow-delimiters paredit racket-mode use-package undo-tree evil-org auto-compile)))
+
+(use-package org-journal
+  :ensure t
+  :defer t
+  :config
+  (setq org-journal-dir "~/org/journal/"
+        org-journal-date-format "%A, %d %B %Y"
+        org-journal-time-format "%I:%M %p"))
+(global-set-key (kbd "C-c o") 'org-journal-new-entry)
+
+(use-package anki-editor)
 
 ;; Must be done before evil
 (use-package undo-tree)
@@ -104,34 +98,21 @@
 (setq evil-undo-system 'undo-tree)
 
 ;; Evil mode options
-(use-package evil-org
+(use-package evil
   :ensure t
-  :after org
+  :init
+  (setq evil-want-keybinding nil
+        evil-vsplit-window-right t)
   :config
-  (add-hook 'org-mode-hook 'evil-org-mode)
-  (add-hook 'evil-org-mode-hook
-            (lambda ()
-              (evil-org-set-key-theme)))
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
+  (evil-mode 1))
 
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
-(setq evil-vsplit-window-right t)
-
-;; key bindings
-(evil-ex-define-cmd "!" 'shell-command)
-;; (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-;; (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-;; (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-;; (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
-(define-key evil-normal-state-map ";" 'buffer-menu)
-
-(with-eval-after-load 'evil
-    (defalias #'forward-evil-word #'forward-evil-symbol)
-    ;; make evil-search-word look for symbol rather than word boundaries
-    (setq-default evil-symbol-word-search t))
+(use-package evil-collection
+  :after evil
+  :config
+  (setq
+   ;; evil-collection-mode-list '(ag dired magit mu4e which-key)
+   evil-collection-want-unimpaired-p nil)
+  (evil-collection-init))
 
 ;; Use evil-surround
 (use-package evil-surround
@@ -139,34 +120,54 @@
   :config
   (global-evil-surround-mode 1))
 
-;; Ivy
-;; (require 'ivy)
-;; (ivy-mode 1)
-;; (counsel-mode 1)
-;; (setq ivy-use-virtual-buffers t)
-;; (setq enable-recursive-minibuffers t)
-;; ;; enable this if you want `swiper' to use it
-;; ;; (setq search-default-mode #'char-fold-to-regexp)
-;; (global-set-key "\C-s" 'swiper)
-;; (global-set-key (kbd "C-c C-r") 'ivy-resume)
-;; (global-set-key (kbd "<f6>") 'ivy-resume)
-;; (global-set-key (kbd "M-x") 'counsel-M-x)
-;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-;; (global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
-;; (global-set-key (kbd "<f1> l") 'counsel-find-library)
-;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-;; (global-set-key (kbd "C-c g") 'counsel-git)
-;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
-;; (global-set-key (kbd "C-c k") 'counsel-ag)
-;; (global-set-key (kbd "C-x l") 'counsel-locate)
-;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-;; (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-;; ;; Custom from reddit.com/r/emacs/comments/6i8rmb
-;; (global-set-key (kbd "C-p") 'ivy-previous-line)
-;; (global-set-key (kbd "C-n") 'ivy-next-line)
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda () (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
+;; Key bindings
+(evil-ex-define-cmd "!" 'shell-command)
+(define-key evil-normal-state-map ";" 'buffer-menu)
+
+;; Calendar mode remains in the hands of Emacs
+(evil-set-initial-state 'calendar-mode 'emacs)
+
+(with-eval-after-load 'evil
+    (defalias #'forward-evil-word #'forward-evil-symbol)
+    ;; make evil-search-word look for symbol rather than word boundaries
+    (setq-default evil-symbol-word-search t))
+
+;; Ivy Configuration
+(require 'ivy)
+(ivy-mode 1)
+(counsel-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+;; enable this if you want `swiper' to use it
+(setq search-default-mode #'char-fold-to-regexp)
+(setq lazy-highlight-cleanup nil)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 
 ;; This is going to switch switching windows to C-up, right, left, down
 ;; (windmove-default-keybindings 'control)
@@ -185,31 +186,17 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/vendor/emacs-color-theme-solarized")
 
 ;; Dracula Theme
-;; (load-theme 'dracula t)
 (set-face-attribute 'org-level-1 nil :height 1.0)
 (set-face-attribute 'org-level-2 nil :height 1.0)
 (set-face-attribute 'org-level-3 nil :height 1.0)
 
 ;; Solarized Theme
-;; (setf solarized-termcolors 256)
-;; (load-theme 'solarized t)
-; (set-frame-parameter nil 'background-mode 'light)
-
-;; (add-hook 'after-make-frame-functions
-;;           (lambda (frame)
-;;             (let ((mode (if (display-graphic-p frame) 'light 'dark)))
-;;               (set-frame-parameter frame 'background-mode 'light)
-;;               (set-terminal-parameter frame 'background-mode 'dark))
-;;             (enable-theme 'solarized)))
-
-;; (add-hook 'after-make-frame-functions
-;;           (lambda (frame)
-;;             (set-terminal-parameter frame 'background-mode 'dark)))
+(setf solarized-termcolors 256)
 
 ;; Different themes for terminal and GUI
 (if (display-graphic-p) 
-    (load-theme 'solarized 1) 
-  (load-theme 'dracula 1))
+    (load-theme 'solarized t)
+  (load-theme 'dracula t))
 
 ; Lisps
 ;; Racket
@@ -233,6 +220,30 @@
                    (setq show-paren-style 'expression)
                    (paredit-mode)
                    (rainbow-delimiters-mode))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7451f243a18b4b37cabfec57facc01bd1fe28b00e101e488c61e1eed913d9db9" default))
+ '(org-agenda-custom-commands
+   '(("n" "Agenda and all TODOs"
+      ((agenda "" nil)
+       (alltodo "" nil))
+      nil)
+     ("i" "Important: Urgent things and phone calls"
+      ((tags "URGENT"
+             ((org-agenda-overriding-header "Urgent things to do")))
+       (tags "PHONE"
+             ((org-agenda-overriding-header "Phone calls to do"))))
+      nil nil)))
+ '(org-agenda-files '("~/org/school.org"))
+ '(org-log-into-drawer t)
+ '(package-selected-packages
+   '(ivy rainbow-delimiters paredit racket-mode use-package undo-tree evil-org auto-compile)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
